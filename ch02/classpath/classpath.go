d8a3d0e94026444f02d7c65a84e7125da59392ba
liweiyuan <535767595@qq.com>
2018-02-01 15:36:17 +0800

    jvmgo 第二章

--- ch02/classpath/classpath.go
+++ ch02/classpath/classpath.go
@@ -1,18 +1,58 @@
 package classpath
 
-type Classpath struct{
+import (
+	"path/filepath"
+	"os"
+)
+
+type Classpath struct {
 	bootClasspath Entry
-	extClasspath Entry
+	extClasspath  Entry
 	userClasspath Entry
 }
 
-func Parse(jreOption,cpOption string) *Classpath{
+func Parse(jreOption, cpOption string) *Classpath {
+	cp := &Classpath{}
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
+}
+func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
+}
+
+func (self *Classpath) String() string {
 
 }
-func (self *Classpath) ReadClass(className string)([]byte,Entry,error){
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	// jre/lib/*
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	// jre/lib/ext/*
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
 
 }
-
-func (self *Classpath) String() string{
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+	if exists("./jre") {
+		return "./jre"
+	}
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+	panic("Can not found jre folder..")
+}
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+func (self *Classpath) parseUserClasspath(cpOption string) {
 
 }
