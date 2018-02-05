419d2d5cf4f9c4b9d5f68723853b739fec4886cf
liweiyuan <535767595@qq.com>
2018-02-05 13:19:06 +0800

    jvmgo 第三章--常量池- or Double

--- ch03/classfile/cp_numeric.go
+++ ch03/classfile/cp_numeric.go
@@ -34,3 +34,35 @@ func (self *ConstantFloatInfo) readInfo(reader *ClassReader) {
 	bytes := reader.readUint32()
 	self.val = math.Float32frombits(bytes)
 }
+
+type ConstantLongInfo struct {
+	val int64
+}
+
+/**
+CONSTANT_Long_info {
+u1 tag;
+u4 high_bytes;
+u4 low_bytes;
+}
+ */
+//readInfo（）先读取一个uint64数据，然后把它转型成int64类型
+func (self *ConstantLongInfo) readInfo(reader *ClassReader) {
+	bytes := reader.readUint64()
+	self.val = int64(bytes)
+}
+
+type ConstantDoubleInfo struct {
+	val float64
+}
+/**
+CONSTANT_Double_info {
+u1 tag;
+u4 high_bytes;
+u4 low_bytes;
+}
+ */
+func (self *ConstantDoubleInfo) readInfo(reader *ClassReader)  {
+	bytes:=reader.readUint64()
+	self.val=math.Float64frombits(bytes)
+}
